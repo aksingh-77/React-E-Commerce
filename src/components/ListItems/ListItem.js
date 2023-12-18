b1b02b687e2ec444bad031df403fab55ce511f26
Abhishek Singh <aksinghpro77@gmail.com>
2023-12-18 13:25:13 +0530

    conditional rendering in component

--- src/components/ListItems/ListItem.js
+++ src/components/ListItems/ListItem.js
@@ -1,7 +1,18 @@
 import AddToCartIcon from "../../assets/icons/add_cart.svg";
-
+import { useState } from "react";
 const ListItem = ({data}) => {
-    // console.log(data);
+    const [counter, setCounter] = useState(0);
+
+    const increasedCounterByOne = () => {
+        setCounter(counter +1);
+    }
+
+    const decreasedCounterByOne = () => {
+        if(counter === 0){
+            return;
+        }
+        setCounter(counter-1);
+    }    
     return (
         <div className={"item-card"}>
             <img className={"img-fluid"} src={`/assets/${data.thumnail}`} alt={data.title}/>
@@ -16,10 +27,21 @@ const ListItem = ({data}) => {
                     <h3>{data.title}</h3>
                 </div>
             </div>
-            <button className={"cart-add"}>
-                <span>Add to Cart</span>
-                <img src={AddToCartIcon} alt="cart Icon"></img>
-            </button>
+            {(
+                counter < 1 ? 
+                <button className={"cart-add"} onClick={increasedCounterByOne}>
+                    <span>Add to Cart</span>
+                    <img src={AddToCartIcon} alt="cart Icon"></img>
+                </button> 
+                : 
+                <div className={"cart-addon"}>
+                    <button onClick={decreasedCounterByOne}><span>-</span></button>
+                    <span className={"counter"}>{counter}</span>
+                    <button onClick={increasedCounterByOne}><span>+</span></button>
+                </div> 
+            )}
+            
+            
         </div>
     )
 }
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,28 +1,49 @@
+import { useState } from "react";
 import ListItem from "../ListItems/ListItem";
 
-const items = [
-    {
-        id:0,
-        title:"Title of this item 1",
-        price: 450,
-        discountedPrice: 340,
-        thumnail: "placeholder.png"
-    },
-    {
-        id:1,
-        title:"Title of this item 2",
-        price: 100,
-        discountedPrice: 80,
-        thumnail: "placeholder.png"
-    }
-]
-
 const Products = ({data}) => {
+    const [items, setItems] = useState([
+        {
+            id:0,
+            title:"Title of this item 1",
+            price: 450,
+            discountedPrice: 340,
+            thumnail: "placeholder.png"
+        },
+        {
+            id:1,
+            title:"Title of this item 2",
+            price: 100,
+            discountedPrice: 80,
+            thumnail: "placeholder.png"
+        },
+        {
+            id:2,
+            title:"Title of this item 3",
+            price: 500,
+            discountedPrice: 480,
+            thumnail: "placeholder.png"
+        },
+        {
+            id:3,
+            title:"Title of this item 4",
+            price: 560,
+            discountedPrice: 500,
+            thumnail: "placeholder.png"
+        }
+    ])
+
     return (
         <div className={"product-list"}>
             <div className={"product-list--wrapper"}>
-                <ListItem data={items[0]} />
-                <ListItem data={items[1]} />
+                {/* <ListItem data={items[0]} />
+                <ListItem data={items[1]} /> */}
+                {
+                    items.map(item=>{
+                        // console.log(item);
+                        return (<ListItem key={item.id} data={item} />)
+                    })
+                }
             </div>
         </div>
     )
